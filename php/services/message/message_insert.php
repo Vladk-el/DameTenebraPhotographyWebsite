e90f7e0ff664cf9fef06b611ad88a611d2700d03
ela <eliott.laversin@connecthings.com>
2015-09-29 09:29:48 +0200

    clean up text && add email fct to post_message

--- php/services/message/message_insert.php
+++ php/services/message/message_insert.php
@@ -8,18 +8,40 @@
     $sql = 'INSERT INTO `message`(`id`, `name`, `website`, `email`, `subject`, `message`, `date`) VALUES (null, "' . $obj->{'name'} . '", "' . $obj->{'website'} . '", "' . $obj->{'email'} . '", "' . $obj->{'subject'} . '", "' . $obj->{'message'} . '", "' . $obj->{'date'} . '")'; 
     
     //print $sql;
+
+    /* Email */
+
+    $mailToContact = "contact@dametenebra.com"; 
+	$mailToError = "dev@dametenebra.com";
+	$br = "\n";
+	$subject = "Mail from dametenebra.com";
+	$subjectError = "Query PROBLEM from dametenebra.com";
+	$body = "Pseudo : " . $obj->{'name'} . $br . 
+			 "Subject : " . $obj->{'subject'} . $br . 
+			 "Site : " . $obj->{'website'} . $br . 
+			 "Mail : " . $obj->{'email'} . $br . 
+			 "Message : " . $obj->{'message'} . $br;
+	$headers = 'From: contact@dametenebra.com' . $br;
+    $headers .= 'Reply-To: contact@dametenebra.com' . $br;
+    $headers .= 'Content-Type: text/plain; charset="utf-8"' . $br;
+    $headers .= 'Content-Transfer-Encoding: 8bit';
+
+    mail($mailToContact, $subject, $body, $headers);
+
+    /* /Email */
     
     try{
         $con = mysqli_connect('localhost', 'root', '', 'dametenebra');
         mysqli_set_charset($con, "utf8");
     }catch (Exception $e){
-        die('Erreur : ' . $e->getMessage());
+        mail($mailToError, $subjectError, $body, $headers);
+        die('Erreur with db : ' . $e->getMessage());
     }
 
     if (mysqli_query($con, $sql)) {
         echo "success";
     } else {
-        echo "Error: " . $sql . "<br />" . mysqli_error($con);
+        echo "Error when adding message to db : " . $sql . "<br />" . mysqli_error($con);
     }
 
     mysqli_close($con);  
