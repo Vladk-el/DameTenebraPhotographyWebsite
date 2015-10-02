69e8d52e94a01943b36fd08e05afb814869ab7d4
ela <eliott.laversin@connecthings.com>
2015-10-02 12:13:57 +0200

    add a photo service to detect bad sized photos

---
+++ php/services/photo/photos_to_resize.php
@@ -0,0 +1,36 @@
+<?php
+    /* Connexion */
+    try{
+        $con = mysqli_connect('localhost', 'root', '', 'dametenebra');
+    }catch (Exception $e){
+      die('Erreur : ' . $e->getMessage());
+    }
+
+    mysqli_set_charset($con, "utf8");
+
+    $response = mysqli_query($con, 'SELECT * FROM photo');
+    $row_count = $response->num_rows;
+    $cpt = 1;
+
+    print("[");
+    while($r = mysqli_fetch_assoc($response)) {
+        
+        
+        list($width, $height) = getimagesize("../../../img/full/" . $r['photo_link']);
+        $r['photo_width'] = $width;
+        $r['photo_height'] = $height;
+        
+        if($width != 600 && $width != 900 && $height != 600 && $height != 900) {
+            print json_encode($r);
+            if($cpt < $row_count) {
+                print(",");
+            }
+        }
+
+        
+        $cpt++;
+    }
+    print("]");
+
+    mysqli_close($con);
+?>
