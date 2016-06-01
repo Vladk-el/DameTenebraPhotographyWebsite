63bde77fb7d95e1bded4089072d9f869608cad95
Vladk-el <eliott.laversin@gmail.com>
2016-06-01 11:45:25 +0200

    update category service to get last img on portfolio

--- php/services/category/categories.php
+++ php/services/category/categories.php
@@ -15,20 +15,20 @@
         if($r != null && $r != "" && strlen(json_encode($r)) > 0 ) {
             
             if(!empty($_GET['display'])) {
-                $photo = mysqli_query($con, 'SELECT * FROM photo WHERE category_photo = ' . $r['category_id'] . ';');
-                if($photo->num_rows > 0){
+                $photos = mysqli_query($con, 'SELECT * FROM photo WHERE category_photo = ' . $r['category_id'] . ' ORDER BY photo_date;');
+                if($photos->num_rows > 0){
                     $imgs = array();
-                    while($img = mysqli_fetch_array($photo)){
+                    while($img = mysqli_fetch_array($photos)){
                         array_unshift($imgs, $img);
                     }
                 }            
 
-                list($width, $height) = getimagesize("../../../img/full/" . $imgs[count($imgs) - 1]['photo_link']);
+                list($width, $height) = getimagesize("../../../img/full/" . $imgs[0]['photo_link']);
 
                 $r['photo_width'] = $width;
                 $r['photo_height'] = $height;
 
-                $r['photo_link'] = $imgs[count($imgs) - 1]['photo_link'];
+                $r['photo_link'] = $imgs[0]['photo_link'];
                 
             }
             
