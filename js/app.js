621e62c875ab74a52de51884f79fe67c008ac538
Vladk-el <eliott.laversin@gmail.com>
2017-03-13 13:01:07 +0100

    move gallery

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function (angular) {
 	'use strict';
 
-	angular.module('ngApp', ['ui.router', 'home', 'links', 'biography', 'contact', 'legalNotice'])
+	angular.module('ngApp', ['ui.router', 'home', 'links', 'biography', 'gallery', 'contact', 'legalNotice'])
 		// , 'indexService', 'portfolioService', 'galleryService', 'linkService', 'contactService'
 
 	.controller('MainController', function ($scope, $location) {
--- js/controller/gallery.js
+++ js/controller/gallery.js
@@ -1,95 +1,102 @@
-(function () {
-    'use strict';
+angular.module('gallery', ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 
-    angular.module('galleryService', ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
+.config(function config($stateProvider) {
+	$stateProvider
+		.state('gallery', {
+			parent: '',
+			url: '/gallery/:category_id',
+			views: {
+				'main@': {
+					controller: 'GalleryController',
+					templateUrl: 'partials/gallery.tpl.html'
+				}
+			},
+			data: {
+				pageTitle: 'Gallery'
+			},
+			resolve: {
+				category: function ($stateParams, $http) {
+					return $http.get('php/services/category/category.php?category=' + $stateParams.category_id);
+				},
+				photos: function ($stateParams, $http) {
+					return $http.get('php/services/photo/photos_by_category.php?category=' + $stateParams.category_id)
+				}
+			}
+		});
+})
 
-    .controller('GalleryController', function ($scope, $http, $routeParams, $log, $filter, $uibModal, $document) {
-        //$log.info('GalleryController');
-        //$log.debug("$routeParams", $routeParams);
+.controller('GalleryController', function ($scope, $http, $log, $uibModal, $document, category, photos) {
+	//$log.info('GalleryController');
 
-        $scope.category_id = $routeParams.category_id;
-        $scope.photo_id = $routeParams.photo_id;
-        $scope.category = {};
-        $scope.photos = [];
-        $scope.images = [];
+	$scope.category = category.data;
+	$scope.photos = photos.data;
+	/*$scope.photo_id = $routeParams.photo_id;*/
 
-        $http.get('php/services/category/category.php?category=' + $scope.category_id)
-            .then(function (response) {
-                $scope.category = response.data;
-                $log.debug(response);
-            })
-            .catch(function (error) {
-                $log.error(error);
-            });
+	var enrichPhotos = function (photos) {
+		var imgs = [];
+		for (var i in $scope.photos) {
+			var img = {};
+			img.url = "img/full/" + photos[i].photo_link;
+			img.title = photos[i].photo_name;
+			img.caption = photos[i].photo_description;
+			imgs.push(img);
+		}
+		return imgs;
+	}
 
-        $http.get('php/services/photo/photos_by_category.php?category=' + $scope.category_id)
-            .then(function (response) {
-                $scope.photos = response.data;
-                for (var i in $scope.photos) {
-                    var img = {};
-                    img.url = "img/full/" + $scope.photos[i].photo_link;
-                    img.title = $scope.photos[i].photo_name;
-                    img.caption = $scope.photos[i].photo_description;
-                    $scope.images.push(img);
-                }
-            })
-            .catch(function (error) {
-                $log.error(error);
-            });
+	$scope.imgs = enrichPhotos(photos.data);
 
-        $scope.show = function (photo, parentSelector) {
-            /*$state.go('.', { photo_id: 1 }, { notify: false });*/
-            var parentElem = parentSelector ?
-                angular.element($document[0].querySelector('.gallery-container ' + parentSelector)) : undefined;
-            var modal = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'partials/templates/modals/viewer.tpl.html',
-                size: 'lg',
-                controller: 'ViewerModalCtrl',
-                appendTo: parentElem,
-                resolve: {
-                    photo: function () {
-                        return photo;
-                    }
-                }
-            });
+	$scope.show = function (photo, parentSelector) {
+		/*$state.go('.', { photo_id: 1 }, { notify: false });*/
+		var parentElem = parentSelector ?
+			angular.element($document[0].querySelector('.gallery-container ' + parentSelector)) : undefined;
+		var modal = $uibModal.open({
+			animation: true,
+			ariaLabelledBy: 'modal-title',
+			ariaDescribedBy: 'modal-body',
+			templateUrl: 'partials/templates/modals/viewer.tpl.html',
+			size: 'lg',
+			controller: 'ViewerModalCtrl',
+			appendTo: parentElem,
+			resolve: {
+				photo: function () {
+					return photo;
+				}
+			}
+		});
 
-            return modal.result
-                .then(function () {
-                    // success
-                }, function () {
-                    // error or cancel
-                    $log.info('modal-component dismissed at: ' + new Date());
-                });
-        }
+		return modal.result
+			.then(function () {
+				// success
+			}, function () {
+				// error or cancel
+				$log.info('modal-component dismissed at: ' + new Date());
+			});
+	}
 
-    })
+})
 
-    .controller('ViewerModalCtrl', function ($scope, $log, $uibModalInstance, photo) {
+.controller('ViewerModalCtrl', function ($scope, $log, $uibModalInstance, photo) {
 
-        $log.debug("Enter ViewerModalCtrl with photo", photo);
+	$log.debug("Enter ViewerModalCtrl with photo", photo);
 
-        $scope.photo = photo;
+	$scope.photo = photo;
 
-        $scope.closeModalDialog = function () {
-            $uibModalInstance.close();
-        }
+	$scope.closeModalDialog = function () {
+		$uibModalInstance.close();
+	}
 
-        $scope.cancelModalDialog = function () {
-            $uibModalInstance.dismiss('cancel');
-        }
+	$scope.cancelModalDialog = function () {
+		$uibModalInstance.dismiss('cancel');
+	}
 
-        $scope.next = function () {
+	$scope.next = function () {
 
-        }
+	}
 
-        $scope.previous = function () {
+	$scope.previous = function () {
 
-        }
-    })
+	}
+})
 
-    ;
-
-})(angular);
+;
