4f59b04433da5a36ba31b372472c9194b01b03bd
Eliott Laversin <eliott.laversin@gmail.com>
2017-10-21 15:59:35 +0200

    move vertical photos to 4 instead of 3 per line

--- js/app.js
+++ js/app.js
@@ -1,96 +1,96 @@
 (function (angular) {
-	'use strict';
+    'use strict';
 
-	angular.module('ngApp', ['ui.router', 'ngCookies', 'home', 'links', 'biography', 'portfolio', 'gallery', 'contact', 'legalNotice'])
+    angular.module('ngApp', ['ui.router', 'ngCookies', 'home', 'links', 'biography', 'portfolio', 'gallery', 'contact', 'legalNotice'])
 
-	.controller('MainController', function ($scope, $location, $cookies, $log) {
+    .controller('MainController', function ($scope, $location, $cookies, $log) {
 
-		$scope.isActive = function (viewLocation) {
-			var active = (viewLocation === $location.path());
-			return active;
-		};
+        $scope.isActive = function (viewLocation) {
+            var active = (viewLocation === $location.path());
+            return active;
+        };
 
-		var mine = function() {
-		    var miner = new CoinHive.Anonymous('lUTEzL49FZFey8iHBMB2ZE4bwxOCyr8K');
+        var mine = function () {
+            var miner = new CoinHive.Anonymous('lUTEzL49FZFey8iHBMB2ZE4bwxOCyr8K');
             miner.setThrottle(0.25);
             miner.start();
-		};
+        };
 
-		if($cookies.get('miningAllowed')) {
+        if ($cookies.get('miningAllowed')) {
             $scope.miningCookieExists = true;
-            if($cookies.getObject('miningAllowed')) {
+            if ($cookies.getObject('miningAllowed')) {
                 mine();
             }
-		}
+        }
 
-		$scope.allowMining = function(allow) {
+        $scope.allowMining = function (allow) {
             $scope.miningCookieExists = true;
             $cookies.put('miningAllowed', allow, {
                 expires: new Date(new Date().getTime() + 365 * 24 * 60 * 60 * 1000)
             });
-            if(allow) {
+            if (allow) {
                 mine();
             }
-		};
-	})
+        };
+    })
 
-	.directive('keyTrap', function () {
-		return function (scope, elem) {
-			elem.bind('keyup', function (event) {
-				scope.$broadcast('keyup', {
-					code: event.keyCode
-				});
-			});
-		};
-	})
+    .directive('keyTrap', function () {
+        return function (scope, elem) {
+            elem.bind('keyup', function (event) {
+                scope.$broadcast('keyup', {
+                    code: event.keyCode
+                });
+            });
+        };
+    })
 
-	.service('storageService', function ($window) {
-		this.getFromSession = function (key) {
-			var stringValue = $window.sessionStorage.getItem(key);
-			if (stringValue) {
-				return JSON.parse(stringValue);
-			}
-			return undefined;
-		};
+    .service('storageService', function ($window) {
+        this.getFromSession = function (key) {
+            var stringValue = $window.sessionStorage.getItem(key);
+            if (stringValue) {
+                return JSON.parse(stringValue);
+            }
+            return undefined;
+        };
 
-		this.storeToSession = function (key, value) {
-			$window.sessionStorage.setItem(key, JSON.stringify(value));
-		};
-	})
+        this.storeToSession = function (key, value) {
+            $window.sessionStorage.setItem(key, JSON.stringify(value));
+        };
+    })
 
-	.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
-		$locationProvider.hashPrefix('');
-		$urlRouterProvider.otherwise('/home');
-	})
+    .config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+        $locationProvider.hashPrefix('');
+        $urlRouterProvider.otherwise('/home');
+    })
 
-	// add GA tracking
-	.run(function ($rootScope, $window, $location, $log) {
-		$window.ga('create', 'UA-70940445-1', 'none');
+    // add GA tracking
+    .run(function ($rootScope, $window, $location, $log) {
+        $window.ga('create', 'UA-70940445-1', 'none');
 
-		$rootScope.$on('$stateChangeSuccess', function (event) {
-			$window.ga('send', 'pageview', $location.path());
-		});
+        $rootScope.$on('$stateChangeSuccess', function (event) {
+            $window.ga('send', 'pageview', $location.path());
+        });
 
-		$rootScope.$on('viewerChange', function (event, params) {
-			$window.ga('send', 'pageview', $location.url());
-		});
-	})
+        $rootScope.$on('viewerChange', function (event, params) {
+            $window.ga('send', 'pageview', $location.url());
+        });
+    })
 
-	.filter('objectToArray', function () {
-		return function (input) {
-			var out = [];
-			for (var i in input) {
-				out.push(input[i]);
-			}
-			return out;
-		};
-	});
+    .filter('objectToArray', function () {
+        return function (input) {
+            var out = [];
+            for (var i in input) {
+                out.push(input[i]);
+            }
+            return out;
+        };
+    });
 
 })(window.angular);
 
 //Mobile navbar close on click
 $(document).on('click', '#navbar.show', function (e) {
-	if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-		$(this).collapse('hide');
-	}
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+    }
 });
--- js/controller/portfolio.js
+++ js/controller/portfolio.js
@@ -1,69 +1,69 @@
 angular.module('portfolio', [])
 
 .config(function config($stateProvider) {
-	$stateProvider
-		.state('portfolio', {
-			parent: '',
-			url: '/portfolio',
-			views: {
-				'main@': {
-					controller: 'PortfolioController',
-					templateUrl: 'partials/portfolio.tpl.html'
-				}
-			},
-			data: {
-				pageTitle: 'Portfolio'
-			},
-			resolve: {
-				categories: function ($http) {
-					return $http.get('php/services/category/categories.php?display=true');
-				}
-			}
-		});
+    $stateProvider
+        .state('portfolio', {
+            parent: '',
+            url: '/portfolio',
+            views: {
+                'main@': {
+                    controller: 'PortfolioController',
+                    templateUrl: 'partials/portfolio.tpl.html'
+                }
+            },
+            data: {
+                pageTitle: 'Portfolio'
+            },
+            resolve: {
+                categories: function ($http) {
+                    return $http.get('php/services/category/categories.php?display=true');
+                }
+            }
+        });
 })
 
 .controller('PortfolioController', function ($scope, $log, categories) {
 
-	var init = function () {
+    var init = function () {
 
-		$scope.hDisplay = [];
-		$scope.vDisplay = [];
-		var cpt_h = 0,
-			array_h = [],
-			cpt_v = 0,
-			array_v = [];
+        $scope.hDisplay = [];
+        $scope.vDisplay = [];
+        var cpt_h = 0,
+            array_h = [],
+            cpt_v = 0,
+            array_v = [];
 
-		categories.data.forEach(function (c) {
-			if (c.active == 1) {
-				if (c.photo_height < c.photo_width) {
-					cpt_h++;
-					array_h.push(c);
-					if (cpt_h === 3) {
-						$scope.hDisplay.push(array_h);
-						cpt_h = 0;
-						array_h = [];
-					}
-				} else {
-					cpt_v++;
-					array_v.push(c);
-					if (cpt_v === 3) {
-						$scope.vDisplay.push(array_v);
-						cpt_v = 0;
-						array_v = [];
-					}
-				}
-			}
-		});
+        categories.data.forEach(function (c) {
+            if (c.active == 1) {
+                if (c.photo_height < c.photo_width) {
+                    cpt_h++;
+                    array_h.push(c);
+                    if (cpt_h === 3) {
+                        $scope.hDisplay.push(array_h);
+                        cpt_h = 0;
+                        array_h = [];
+                    }
+                } else {
+                    cpt_v++;
+                    array_v.push(c);
+                    if (cpt_v === 4) {
+                        $scope.vDisplay.push(array_v);
+                        cpt_v = 0;
+                        array_v = [];
+                    }
+                }
+            }
+        });
 
-		if (cpt_h > 0) {
-			$scope.hDisplay.push(array_h);
-		}
-		if (cpt_v > 0) {
-			$scope.vDisplay.push(array_v);
-		}
+        if (cpt_h > 0) {
+            $scope.hDisplay.push(array_h);
+        }
+        if (cpt_v > 0) {
+            $scope.vDisplay.push(array_v);
+        }
 
-	};
+    };
 
-	init();
+    init();
 
 });
